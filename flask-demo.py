d7c8cc9a94dc8094f6abcfd55f4402796701952f
Aiden Chia <aidenchia95@gmail.com>
2019-02-15 09:53:07 +0800

    adding secret key

--- flask-demo.py
+++ flask-demo.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask import Flask, flash, redirect, render_template, request, session, abort
 import os
- 
+
 app = Flask(__name__)
  
 @app.route('/')
@@ -24,7 +24,13 @@ def do_admin_login():
 def logout():
   session['logged_in'] = False
   return home()
+
+@app.route('/course-details', methods=['GET','POST'])
+def do_prof_input():
+  return render_template('courseDetails.html')
+
  
 if __name__ == "__main__":
   app.secret_key = os.urandom(12)
+  app.config['SESSION_TYPE'] = 'filesystem'
   app.run(debug=True,host='0.0.0.0', port=5000)
